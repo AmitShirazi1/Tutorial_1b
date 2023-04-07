63076419b8c8513d4be74e263de9d9616de9e767
Amit <amit.shirazi@campus.technion.ac.il>
2023-04-08 00:46:37 +0300

    Adding files

---
+++ basic-pointer-practice.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int var1;
+    char var2[10];
+
+    printf("The address of var1 is: %p\n", &var1);
+    printf("The address of var2 is: %p\n", &var2);
+
+    return 0;
+}
