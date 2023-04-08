02fa48ccd103c428018c0769ecf8fdae50832b03
Amit <amit.shirazi@campus.technion.ac.il>
2023-04-08 11:54:54 +0300

    Header in progress

--- LinkedList/linked-list.h
+++ LinkedList/linked-list.h
@@ -1,6 +1,19 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+typedef struct Student {
+    int id;
+    char name[10];
+} Student;
 
+typedef struct Node {
+    Student student;
+    LNode next;
+} LNode;
+
+typedef struct DoubleLinkedList {
+    LNode left;
+    LNode right;
+} DLlist;
 
 #endif
