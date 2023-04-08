8e4afaaf33d3686eb9f26671ea0ea12c3f109cac
Amit <amit.shirazi@campus.technion.ac.il>
2023-04-08 12:08:47 +0300

    Insert command

--- LinkedList/linked-list.h
+++ LinkedList/linked-list.h
@@ -7,16 +7,18 @@ typedef struct Student {
 } Student;
 
 typedef struct Node {
-    Student student;
-    LNode next;
+    Student *student;
+    LNode *next;
 } LNode;
 
 typedef struct DoubleLinkedList {
-    LNode left;
-    LNode right;
+    LNode *left;
+    LNode *right;
 } DLlist;
 
 #define LEFT 0
 #define RIGHT 1
+
+void insert(DLlist *list, Student *student, int side);
 
 #endif
